241285de9fa2366364392aa200125c51613b8826
unknown <maron.a.h.92@gmail.com>
2021-01-04 02:03:15 +0200

    changing tests module name

--- flight/src/test/java/com/example/flight/ControllerTest.java
+++ flight/src/test/java/com/example/flight/ControllerTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(TicketController.class)
-class TicketControllerTest {
+class ControllerTest {
     @Autowired
     private MockMvc mvc;
     public AppData data = new AppData();
